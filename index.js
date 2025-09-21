e1fefb5e28ff219334f9fbe46b2ea4277557bfa5
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-09-21 23:05:46 +0800

    added highlighting of leadeR

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 let homeScoreEl = document.getElementById("home-score")
 let awayScoreEl = document.getElementById("away-score")
+let homeBoardEl = document.getElementById("home-board")
+let awayBoardEl = document.getElementById("away-board")
 let homeScore = 0
 let awayScore = 0
 
 function incrementHomeScore(value) {
     homeScore += value
     homeScoreEl.textContent = homeScore
+    highlightLeader()
 }
 
 function incrementAwayScore(value) {
     awayScore += value
     awayScoreEl.textContent = awayScore
+    highlightLeader()
 }
 
 function resetScore() {
@@ -18,4 +22,16 @@ function resetScore() {
     awayScore = 0
     homeScoreEl.textContent = homeScore
     awayScoreEl.textContent = awayScore
+    highlightLeader()
+}
+
+function highlightLeader() {
+    homeBoardEl.style.border = '3px solid #080001';
+    awayBoardEl.style.border = '3px solid #080001';
+
+    if (homeScore > awayScore) {
+        homeBoardEl.style.border = '3px solid gold';
+    } else if (awayScore > homeScore) {
+        awayBoardEl.style.border = '3px solid gold';
+    }
 }
